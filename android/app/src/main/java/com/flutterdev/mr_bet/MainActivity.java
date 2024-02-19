09b0221cbde7fc463a4d951c3445340c94c42718
Muhammad Ansam Naveed <88399321+Hamza8486@users.noreply.github.com>
2024-02-19 10:49:32 +0500

    latest code
    latest ccode

--- android/app/src/main/java/com/flutterdev/mr_bet/MainActivity.java
+++
@@ -1,9 +0,0 @@
-package com.flutterdev.mr_bet;
-
-import io.flutter.embedding.android.FlutterFragmentActivity;
-import io.flutter.embedding.engine.FlutterEngine;
-import io.flutter.plugins.GeneratedPluginRegistrant;
-
-public class MainActivity extends FlutterFragmentActivity {
-
-}
